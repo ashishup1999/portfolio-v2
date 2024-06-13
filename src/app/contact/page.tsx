95c5f123224df36a093072cc9d6a0a7503f16d02
Ashish Upadhyay <ashishup1999@gmail.com>
2024-06-13 11:58:30 +0530

    contact section added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
 import React from "react";
+import "@/app/contact/contact.scss";
+import myImg from "@/assets/MyImage.png";
+import { Iceland } from "next/font/google";
+
+const iceland = Iceland({ weight: "400", subsets: ["latin"] });
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div id="contact-sec">
+      <Image src={myImg} alt="myImg" />
+      <label className="name">Name</label>
+      <input type="text" className="name" />
+      <label className="email">Email</label>
+      <input type="text" className="email" />
+      <label className="msg">Message</label>
+      <textarea className="msg" />
+      <button id="send" className={iceland.className}>
+        Send
+      </button>
+    </div>
+  );
 };
 
 export default Contact;
